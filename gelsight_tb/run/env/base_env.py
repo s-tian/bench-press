04d5540e38cf8304aadeec8e77ba98222bfc54a4
Stephen Tian <stephentian@berkeley.edu>
2020-01-28 14:02:37 -0800

    Add envs

---
+++ gelsight_tb/run/env/base_env.py
@@ -0,0 +1,12 @@
+class BaseEnv:
+
+    def __init__(self, env_config, logger):
+        self.config = env_config
+        self.logger = logger
+
+    def step(self, action):
+        raise NotImplementedError
+
+    def get_obs(self):
+        raise NotImplementedError
+
