c01ee6fa170aadd27760287e9b2a58aaecdbea45
Stephen Tian <stephentian@berkeley.edu>
2020-01-29 18:53:40 -0800

    Clean up root dir

--- test_dynamixel.py
+++
@@ -1,9 +0,0 @@
-from tb_control.dynamixel_interface import Dynamixel
-import ipdb
-ipdb.set_trace()
-d = Dynamixel('/dev/ttyUSB0', 2900)
-d.move_to_angle(-60)
-cur_pos = d.get_pos()
-print((cur_pos - 2900) * 360 / 4096)
-#new_pos = d.get_pos() - 25 
-
