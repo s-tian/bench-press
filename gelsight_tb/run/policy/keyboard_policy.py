8808049922fb301baa940b8ab829f9358855cd08
Stephen Tian <stephen.st123@gmail.com>
2020-02-02 15:54:23 -0800

    Update camera functionality

--- gelsight_tb/run/policy/keyboard_policy.py
+++ gelsight_tb/run/policy/keyboard_policy.py
@@ -1,7 +1,7 @@
 from gelsight_tb.run.policy.base_policy import BasePolicy
 from gelsight_tb.run.actions.action import DeltaAction, DynamixelAngleAction
 import getch
-
+import cv2
 
 class KeyboardPolicy(BasePolicy):
 
@@ -19,5 +19,14 @@ class KeyboardPolicy(BasePolicy):
     def get_action(self, observation, num_steps):
         ch = None
         while ch not in self.INPUTS:
+            if ch == 'v':
+                self.visualize_observations(observation)
             ch = getch.getch()
         return self.INPUTS[ch]
+
+    @staticmethod
+    def visualize_observations(self, observation):
+        images = observation['images']
+        for name, image in images:
+            cv2.imshow(name, image)
+            cv2.waitKey()
--- gelsight_tb/tb_control/dynamixel_interface.py
+++ gelsight_tb/tb_control/dynamixel_interface.py
@@ -49,31 +49,29 @@ else:
 
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 
-# Control table address
-ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
-ADDR_PRO_GOAL_POSITION      = 116
-ADDR_PRO_PRESENT_POSITION   = 132
-
-# Protocol version
-PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
-
-# Default setting
-DXL_ID                      = 1                 # Dynamixel ID : 1
-BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
-
-TORQUE_ENABLE               = 1                 # Value for enabling the torque
-TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 10                # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 4000              # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
-
-dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
-TICKS_PER_REV = 4096
-
 
 class Dynamixel():
+
+    # Control table address
+    ADDR_PRO_TORQUE_ENABLE = 64  # Control table address is different in Dynamixel model
+    ADDR_PRO_GOAL_POSITION = 116
+    ADDR_PRO_PRESENT_POSITION = 132
+
+    # Protocol version
+    PROTOCOL_VERSION = 2.0  # See which protocol version is used in the Dynamixel
+
+    # Default setting
+    DXL_ID = 1  # Dynamixel ID : 1
+    BAUDRATE = 57600  # Dynamixel default baudrate : 57600
+
+    TORQUE_ENABLE = 1  # Value for enabling the torque
+    TORQUE_DISABLE = 0  # Value for disabling the torque
+    DXL_MINIMUM_POSITION_VALUE = 10  # Dynamixel will rotate between this value
+    DXL_MAXIMUM_POSITION_VALUE = 4000  # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+    DXL_MOVING_STATUS_THRESHOLD = 20  # Dynamixel moving status threshold
+
+    dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]  # Goal position
+    TICKS_PER_REV = 4096
 
     def __init__(self, device_name, home_pos):
 
@@ -85,7 +83,7 @@ class Dynamixel():
         # Initialize PacketHandler instance
         # Set the protocol version
         # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-        self.packetHandler = PacketHandler(PROTOCOL_VERSION)
+        self.packetHandler = PacketHandler(self.PROTOCOL_VERSION)
 
         # Open port
         if self.portHandler.openPort():
@@ -98,7 +96,7 @@ class Dynamixel():
 
 
         # Set port baudrate
-        if self.portHandler.setBaudRate(BAUDRATE):
+        if self.portHandler.setBaudRate(self.BAUDRATE):
             print("Succeeded to change the baudrate")
         else:
             print("Failed to change the baudrate")
@@ -107,7 +105,7 @@ class Dynamixel():
             quit()
 
         # Enable Dynamixel Torque
-        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.DXL_ID, self.ADDR_PRO_TORQUE_ENABLE, self.TORQUE_ENABLE)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -118,12 +116,12 @@ class Dynamixel():
         self.home_pos = home_pos
 
     def move_to_angle(self, angle):
-        target_pos = (int) ((angle / 360) * TICKS_PER_REV + self.home_pos)
+        target_pos = (int) ((angle / 360) * self.TICKS_PER_REV + self.home_pos)
         self.set_pos(target_pos)
 
     def set_pos(self, position):
         # Write goal position
-        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, DXL_ID, ADDR_PRO_GOAL_POSITION, position)
+        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, self.DXL_ID, self.ADDR_PRO_GOAL_POSITION, position)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
             raise ValueError('communication failure!')
@@ -131,7 +129,7 @@ class Dynamixel():
             print("%s" % self.packetHandler.getRxPacketError(dxl_error))
 
     def get_pos(self):
-        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, DXL_ID, ADDR_PRO_PRESENT_POSITION)
+        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, self.DXL_ID, self.ADDR_PRO_PRESENT_POSITION)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -141,11 +139,11 @@ class Dynamixel():
 
     def get_current_angle(self):
         current_ticks = self.get_pos()
-        return (current_ticks - self.home_pos) * 360 / TICKS_PER_REV
+        return (current_ticks - self.home_pos) * 360 / self.TICKS_PER_REV
 
     def disable(self):
         # Disable Dynamixel Torque
-        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, self.DXL_ID, self.ADDR_PRO_TORQUE_ENABLE, self.TORQUE_DISABLE)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
